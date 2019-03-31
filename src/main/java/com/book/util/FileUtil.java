6b902da78b787eebd10acea2e431796789743769
Syed Mainul Hasan <mainuls18@gmail.com>
2019-04-01 00:20:26 +0600

    image reading and rendering methods

--- src/main/java/com/book/util/FileUtil.java
+++ src/main/java/com/book/util/FileUtil.java
@@ -1,8 +1,15 @@
 package com.book.util;
 
+import com.book.service.AttachmentService;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletContext;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Paths;
 
 @Component
@@ -16,6 +23,4 @@ public class FileUtil extends AppBase{
         System.out.println("Making directory: "+dir.getAbsolutePath());
         return dir;
     }
-
-
 }
