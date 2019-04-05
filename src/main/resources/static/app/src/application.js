c0317c00d83563406c98fd2e53f7f964aab24586
Syed Mainul Hasan <mainuls18@gmail.com>
2019-04-05 10:59:48 +0600

    SPA routing improved

--- src/main/resources/static/app/src/application.js
+++ src/main/resources/static/app/src/application.js
@@ -2,6 +2,14 @@ application = (function () {
     return {
         initialize: function () {
             App.RequestManager.handleRouting();
+            if (document.querySelector("requestPath") !== null) {
+                var path = document.querySelector("requestPath").getAttribute("path");
+                App.RequestManager.loadContent(document.querySelector("body"), path, function (container, data) {
+                    var contentPane = container.querySelector(".content-pane")
+                    contentPane.innerHTML = data
+                });
+            }
+            history.pushState(document.location.pathname, 'Admin Dashboard', document.location.pathname);
         }
     }
 }().initialize());
